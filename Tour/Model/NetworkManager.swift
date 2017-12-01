ee1ec643ad58dedfbbdf648ce5d8ef2bb283a00d
una <una@ubivelox.com>
2017-12-01 12:52:19 +0900

    first

---
+++ Tour/Model/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Tour
+//
+//  Created by una on 2017. 12. 1..
+//  Copyright © 2017년 una. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+
+}
